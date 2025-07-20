e243015ae11c18beedacab45a6d165a3057481bd
Jacob Kelly <jskelly2021@gmail.com>
2025-07-19 23:41:06 -0500

    Main function queries user for input

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,13 @@
 #include <printf.h>
+#include <iostream>
 
 int main() {
-    printf("Hello, world!\n");
+    int num_servers = 0;
+    int run_time = 0;
+
+    printf("Enter the number of servers: ");
+    std::cin >> num_servers;
+
+    printf("Enter the load balancer run time (clock cycles): ");
+    std::cin >> run_time;
 }
