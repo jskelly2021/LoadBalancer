766df71fc05a24ecb95fed657237c5eea34b3076
Jacob Kelly <jskelly2021@gmail.com>
2025-07-20 14:11:49 -0500

    Generated Doxygen comments for the Request struct

--- src/request.h
+++ src/request.h
@@ -2,9 +2,31 @@
 
 #include <string>
 
+/**
+ * @struct Request
+ * @brief Represents a unit of work or a simulated web request in the load balancer system.
+ *
+ * Each request contains a unique ID, source and destination IP addresses,
+ * and the time (in clock cycles) required to process the request.
+ */
 struct Request {
+    /**
+     * @brief Unique identifier for the request.
+     */
     int id;
+
+    /**
+     * @brief IP address of the incoming request source.
+     */
     std::string ip_in;
+
+    /**
+     * @brief IP address of the outgoing destination.
+     */
     std::string ip_out;
+
+    /**
+     * @brief Amount of time (in clock cycles) required to process the request.
+     */
     int time;
 };
